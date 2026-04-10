b3eebc7490189b75a22dc927968397db86e83131
suhail-256 <Suhail.Kamal256@gmail.com>
2026-04-10 19:52:32 +0200

    Add middleware for request logging and error handling

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import abiRouter from "./router/abiRouter";
+import middleware from "./utils/middleware";
 
 const app = express();
 
+app.use(middleware.requestLogger);
 app.use(express.static("dist"));
 app.use(express.json());
 
 app.use("/api/abi", abiRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 export default app;
---
+++ server/utils/middleware.ts
@@ -0,0 +1,24 @@
+import morgan from "morgan";
+import express from "express";
+
+const requestLogger = morgan(":method :url :status :response-time ms");
+
+const unknownEndpoint = (req: express.Request, res: express.Response) => {
+	res.status(404).json({
+		error: "unknown endpoint",
+	});
+};
+
+const errorHandler = (
+	err: Error,
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction,
+) => {
+	if (err.name == "SyntaxError") {
+		return res.status(400).json({ error: "Error: Invalid JSON" });
+	}
+	res.status(500).json({ error: "Internal Server Error" });
+};
+
+export default { requestLogger, unknownEndpoint, errorHandler };
